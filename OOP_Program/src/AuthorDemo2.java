baab6efe2c8b161b1e6db022ba291f055f05cd2f
ImyourShin <bu.tachin_st@tni.ac.th>
2023-01-19 16:05:31 +0700

    Exercise [19-1-2023]

--- OOP_Program/src/AuthorDemo2.java
+++ OOP_Program/src/AuthorDemo2.java
@@ -4,13 +4,14 @@ public class AuthorDemo2 {
 	public static void main(String[] args) {
 		Author author[] =new Author[4];
 		for(int i =0;i<author.length;i++) {
-			System.out.println("Information Author "+i+1);
+			System.out.println("Information Author "+(i+1));
 			System.out.println("-------------------------------------------------");
-			System.out.print("Input author name  :");
+			System.out.print("Input author name  : ");
 			String name = scanner.nextLine();
-			System.out.print("Input author email  :");
+			System.out.print("Input author email : ");
 			String email = scanner.nextLine();
 			System.out.println("-------------------------------------------------");
+			author[i] =new Author(name,email);
 		}
 		System.out.println();
 		int i = 1;
